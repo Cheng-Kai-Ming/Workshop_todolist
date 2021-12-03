f4c25fa03ee464e3d72d34d0cd601fb8ccabf196
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2021-12-03 15:24:41 +0800

    task_@

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,29 @@
 // TO DO
+// TO DO
+document.addEventListener("DOMContentLoaded", () => {
+  const close = document.querySelector(".close")
+  const lis = document.querySelectorAll("li")
+  const list = document.querySelector("li")
+  const span = document.querySelector(".close")
+  const ul = document.querySelector("ul")
+
+  lis.forEach(li => li.addEventListener("click", () => {
+    if (li.className !== "checked") {
+      li.className = "checked"
+    }
+  })
+  )
+  ul.addEventListener("click", (e) => {
+    if (e.target.nodeName === "SPAN") {
+      e.target.parentNode.remove()
+    }
+
+
+
+  })
+  //console.log(list.innerHTML)
+  //console.log(span)
+
+
+})
+

7cf5d6bfe8ddcef3691adf2831c2b257d8948bd9
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2021-12-03 12:06:14 +0800

    task_1

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,12 @@
 // TO DO
+document.addEventListener("DOMContentLoaded", () => {
+  const close = document.querySelector(".close")
+  const lis = document.querySelectorAll("li")
+  lis.forEach(li => li.addEventListener("click", () => {
+    if (li.className !== "checked") {
+      li.className = "checked"
+    }
+
+  })
+  )
+})
